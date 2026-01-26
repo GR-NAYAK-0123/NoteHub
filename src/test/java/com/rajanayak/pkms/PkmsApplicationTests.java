5ad61f8f885e405a822418ec50747817b4d23749
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-01-27 01:00:53 +0530

    Spring Boot PKMS project setup

---
+++ src/test/java/com/rajanayak/pkms/PkmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajanayak.pkms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
